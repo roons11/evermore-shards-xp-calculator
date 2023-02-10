55a0cab63d835583191b5b3bb74891c43f182ed0
roons11 <76991539+roons11@users.noreply.github.com>
2023-02-09 22:04:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+const currentLvlInput = document.querySelector(".current-lvl");
+const targetLvlInput = document.querySelector(".target-lvl");
+
+const XpDifference = document.querySelector(".xp-difference .value");
+
+const calculateBtn = document.querySelector(".calculate-btn");
+
+let currentLVL = parseFloat(currentLvlInput.value);
+let targetLVL = parseFloat(targetLvlInput.value);
+
+const calculateXPD = (x,y) => {
+
+    // Declare an array of undefined size
+    var arr = [];
+	var cur = [];
+
+    var x = currentLVL;
+
+    var y = targetLVL;
+
+    // Initiating loop to iterate
+    // for inputs
+    for (var i = 0; i < y+1; i++) { 
+  
+        // Feeding integer inputs to array
+        arr[i] = Math.floor(200 * (i ** (1 + i/100)));
+          
+        // Calculate sum of array elements
+        var sum = arr.reduce(function (a, b) {
+            return a + b;
+        }, 0)
+    }
+	
+	for (var i = 0; i < x+1; i++) { 
+  
+        // Feeding integer inputs to array
+        cur[i] = Math.floor(200 * (i ** (1 + i/100)));
+          
+        // Calculate sum of array elements
+        var dum = cur.reduce(function (a, b) {
+            return a + b;
+        }, 0)
+    }	
+	
+    let xpd = sum - dum;
+
+    return xpd;
+
+    console.log(xpd);
+}
+
+const updateData = (xpd) => {
+    XpDifference.innerHTML = Math.round(xpd);
+}
+
+const refreshInputValues = () => {
+    currentLVL = parseFloat(currentLvlInput.value);
+    targetLVL = parseFloat(targetLvlInput.value);
+}
+
+const inti = () => {
+    refreshInputValues();
+    let xpd = calculateXPD();
+    updateData(xpd);
+}
+
+inti();
+
+calculateBtn.addEventListener("click", inti);
