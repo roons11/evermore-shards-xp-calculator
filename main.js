e9aaa96f86380237d9a024a47a6975863064c2ea
roons11 <76991539+roons11@users.noreply.github.com>
2023-02-13 23:09:53 -0500

    Current XP, XP per craft, Crafts needed added

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const currentLvlInput = document.querySelector(".current-lvl");
 const targetLvlInput = document.querySelector(".target-lvl");
+const currentXPInput = document.querySelector(".current-xp");
+const crafttXPInput = document.querySelector(".craft-xp");
 
 const XpDifference = document.querySelector(".xp-difference .value");
+const CraftDifference = document.querySelector(".crafts-needed .value");
 
 const calculateBtn = document.querySelector(".calculate-btn");
 
 let currentLVL = parseFloat(currentLvlInput.value);
 let targetLVL = parseFloat(targetLvlInput.value);
+let currentXP = parseFloat(currentXPInput.value);
+let craftXP = parseFloat(crafttXPInput.value);
 
-const calculateXPD = (x,y) => {
+
+const calculateXPD = (x,y,z) => {
+    checkValues();
+    refreshInputValues();
 
     // Declare an array of undefined size
     var arr = [];
 	var cur = [];
 
     var x = currentLVL;
-
     var y = targetLVL;
+    var z = currentXP;
 
     // Initiating loop to iterate
     // for inputs
@@ -42,27 +50,74 @@ const calculateXPD = (x,y) => {
         }, 0)
     }	
 	
-    let xpd = sum - dum;
+    let xpd = sum - dum - z;
 
     return xpd;
 
 }
 
-const updateData = (xpd) => {
+const checkValues = () => {
+    let currentLvlvalue = currentLvlInput.value;
+    let targetLvlvalue = targetLvlInput.value;
+    let currentXPvalue = currentXPInput.value;
+    let craftXPvalue = crafttXPInput.value;
 
-    XpDifference.innerHTML = xpd.toLocaleString("en-US");
+    let regexNumber = /^[0-9]+$/;
+
+    if(!currentLvlvalue.match(regexNumber) || !targetLvlvalue.match(regexNumber) || !currentXPvalue.match(regexNumber)) {
+        currentLvlInput.value = "0";
+        targetLvlInput.value = "1";
+        currentXPInput.value = "0";
+        alert("Why are you trying to break my calculator?");
+    }
+
+    if(!craftXPvalue.match(regexNumber)) {
+        crafttXPInput.value = "1";
+        alert("Why are you trying to break my calculator?");
+    }
+
+    if(craftXPvalue < 1) {
+        crafttXPInput.value = "1";
+        alert("STOP! Dividing by 0 destroys the universe!");
+    }
+
+    if(currentLvlvalue > 400 || targetLvlvalue > 400) {
+        currentLvlInput.value = "0";
+        targetLvlInput.value = "1";
+        alert("If you are actually aiming for this level you are spending too much time gaming");
+    }
+
+    if(targetLvlvalue <= currentLvlvalue) {
+        targetLvlInput.value = Math.abs(currentLvlvalue - currentLvlvalue - currentLvlvalue - 1);
+        if(currentLvlInput.value < 0) {
+            currentLvlInput.value = "0";
+            targetLvlInput.value = "1";
+        }
+    }
 
 }
 
 const refreshInputValues = () => {
     currentLVL = parseFloat(currentLvlInput.value);
     targetLVL = parseFloat(targetLvlInput.value);
+    currentXP = parseFloat(currentXPInput.value);
 }
 
 const inti = () => {
-    refreshInputValues();
     let xpd = calculateXPD();
-    updateData(xpd);
+    if(xpd < 0){
+        XpDifference.innerHTML = "Error: Current XP too high";
+    }else{
+        updateData(xpd,crafttXPInput.value);        
+    }
+}
+
+const updateData = (xpd,xpc) => {
+    
+    XpDifference.innerHTML = xpd.toLocaleString("en-US");
+    let j = Math.ceil(xpd/xpc);
+    CraftDifference.innerHTML = j.toLocaleString("en-US");
+
 }
 
 inti();
